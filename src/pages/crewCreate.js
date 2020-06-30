aa3474108a3a5514642ed89fba68216bf22d298e
th0532 <kkllpp123456@naver.com>
2020-06-30 23:56:47 +0900

    Add crewCreate inputFlag

--- src/pages/crewCreate.js
+++ src/pages/crewCreate.js
@@ -6,10 +6,10 @@ const crewCreate = () => {
     return(
         <div className={"content"}>
             <div className={"crewCreate"}>
-                <Category></Category>
-                <Area></Area>
-                <Recruit></Recruit>
-                <Writing></Writing>
+                    <Category></Category>
+                    <Area></Area>
+                    <Recruit></Recruit>
+                    <Writing></Writing>
             </div>
         </div>
     )
@@ -52,17 +52,28 @@ const Area = () => {
 }
 
 const Recruit = () =>{
+
+    const [personnelFlag, setPersonnelFlag] = useState(false);
+    const [mettingDateFlag, setMettingDateFlag] = useState(false);
+    const handleFlag = (type) =>{
+        if(type === "personnel"){
+            setPersonnelFlag(!personnelFlag)
+        }else if(type === "mettingDate"){
+            setMettingDateFlag(!mettingDateFlag)
+        }
+        
+    }
     return(
         <div className={"recruit"}>
             <div className={"personnel"}>
                 <h1>인원</h1>
-                제한 없음<input type="checkbox"/>
-                <input type="range" min="0" max="50"/>
+                제한 없음<input type="checkbox" onClick={() => handleFlag("personnel")}/>
+                {personnelFlag === false && <input type="range" min="0" max="50"/>}
             </div>
             <div className={"metting_date"}>
                 <h1>모임날짜</h1>
-                미정<input type="checkbox"/>
-                <input type="date" />
+                미정<input type="checkbox" onClick={() => handleFlag("mettingDate")}/>
+                {mettingDateFlag === false && <input type="date" placeholder="연도-월-일"/>}
             </div>
         </div>
     )
@@ -94,7 +105,7 @@ const Writing = () => {
                 <textarea type="text" placeholder="내용을 입력해주세요"></textarea>
             </div>
             <div className={"thumbnail"}>
-                <h1>썸네일 이미지</h1>
+                <h1>썸네일 이미지 (선택)</h1>
                 <input type='file' 
                         accept='image/jpg,impge/png,image/jpeg,image/gif' 
                         name='profile_img' 
