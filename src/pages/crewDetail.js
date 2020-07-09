ad33e493df995c65d15070eb437ffa68bda678b8
th0532 <kkllpp123456@naver.com>
2020-07-09 13:25:47 +0900

    Update CrewDetail

--- src/pages/crewDetail.js
+++ src/pages/crewDetail.js
@@ -4,15 +4,24 @@ import "./css/crewDetail.css";
 const CrewDetail = (props) => {
     const data = {
                     imgLink:"https://cdn.pixabay.com/photo/2016/08/19/14/45/books-1605416__340.jpg",
-                    desc_title:"스터디 모임",
-                    desc_content:"매주 목요일 7시 OOO 스터디 할사람 모집합니다.",
+                    desc_title:"골프 치러 갈 사람 구해요",
+                    desc_content:"테스트 텍스트입니다. 테스트 텍스트입니다. 테스트 텍스트입니다. 테스트 텍스트입니다. 테스트 텍스트입니다. 테스트 텍스트입니다. 테스트 텍스트입니다. 테스트 텍스트입니다. 테스트 텍스트입니다. ",
                     list_index:1
                 }
 
     return(
         <div className={"crewDetail"}>
             <div className={"crewDetail_wrap"}>
-            <h1>crew 게시글 상세 페이지 준비중 입니다</h1>
+                <div className={"crewDetail_info"}>
+                    <div className={"crewDetail_desc"}>
+                        <h3>제목: {data.desc_title}</h3>
+                        <p><span className={"sub_title"}>작성자:</span> OOOOO</p>
+                        <p><span className={"sub_title"}>모임 장소:</span> 동작구</p>
+                        <p><span className={"sub_title"}>모임 날짜:</span> 7월 21일</p>
+                        <p><span className={"sub_title"}>내용:</span> {data.desc_content}</p>
+
+                    </div>
+                </div>
             </div>
         </div>
     )
