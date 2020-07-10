05d04f2024e2d2653e21647246f982ccce1d9024
th0532 <kkllpp123456@naver.com>
2020-07-11 02:41:17 +0900

    Updates

---
+++ precache-manifest.8f6db7f81b4ded168d0a708b28f9ef0c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "89894e578ca3f3611c8a6540885f1704",
+    "url": "/peach_web/index.html"
+  },
+  {
+    "revision": "961e8111043b4a2c2e4e",
+    "url": "/peach_web/static/css/2.9e5c3921.chunk.css"
+  },
+  {
+    "revision": "c85eba5403cef4c4aec9",
+    "url": "/peach_web/static/css/main.9d552bf5.chunk.css"
+  },
+  {
+    "revision": "961e8111043b4a2c2e4e",
+    "url": "/peach_web/static/js/2.999e0d69.chunk.js"
+  },
+  {
+    "revision": "3a5402f5ddd536a8f5deb8d906cf7b41",
+    "url": "/peach_web/static/js/2.999e0d69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c85eba5403cef4c4aec9",
+    "url": "/peach_web/static/js/main.405226a0.chunk.js"
+  },
+  {
+    "revision": "68ea17539c8838aae5ce",
+    "url": "/peach_web/static/js/runtime-main.f20de25d.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/peach_web/precache-manifest.b34091b35f81ac64871259ecaacb9aa5.js"
+  "/peach_web/precache-manifest.8f6db7f81b4ded168d0a708b28f9ef0c.js"
 );
 
 self.addEventListener('message', (event) => {
